d6651e65e683bab992e9b82ee7da3a3a5c2446d6
Horacio Acosta <horooo65@gmail.com>
2020-05-05 11:13:27 -0500

    Update Lexer.ex

--- compilador/Lexer.ex
+++ compilador/Lexer.ex
@@ -62,39 +62,28 @@ defmodule Lexer do
   def lexer_principal(lista_principal, lista_tokens) do
     len_temp = length(lista_principal)
 
-    #verifica que la lista ingresada este vacía
     if len_temp > 0 do
-      #lista auxiliar que contien el primer elemento de la lista ingresada
       lista_aux = hd(lista_principal)
-      #se elimina el primer elemento de la lista ingresada 
       lista_principal = List.delete_at(lista_principal, 0)
       cadena_aux = tl(lista_aux)
       cadena_aux = hd(cadena_aux)
       numRenglon = hd(lista_aux)
       lista_aux = {numRenglon,cadena_aux}
-      #el primer elemento de la lista ingresada se compara con los tokens predefinidos
       lista_aux = Lexer.tokens(lista_aux)
-      # a la lista de tokens se le va agregando los elementos ya comparados
       lista_tokens = lista_tokens ++ lista_aux
-      # la función es recursiva hasta que la lista ingresada este vacía
       Lexer.lexer_principal(lista_principal, lista_tokens)
     else
-      #antes de imprimir la lista de tokens se verifica que no haya errores.
-      if errores(lista_tokens) == true do # si la funcion manda un true entonces no hay errores
+      if errores(lista_tokens) == true do
         lista_tokens
       else
-        errores(lista_tokens) #de lo contrario vuelve a llamar a la funcion
+        errores(lista_tokens) 
       end
     end
   end
 
-  #recibe una lista de tuplas
   def errores (lista_tokens) do 
-    #verifica que la lista final no sea vacía
     if lista_tokens != [] do 
-      #divide la lista en cabeza y cola
       [ini | fin] = lista_tokens 
-      #compara el token de la tupla con el atomo de error
       if elem(ini,1) == :error_token do  
         {:error, "linea: #{elem(ini,0)}; #{elem(ini,1)} no definido"}
       else
