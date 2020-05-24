904068f264e46a2cafcd6415e69d815e157873c6
Horacio Acosta <horooo65@gmail.com>
2020-05-23 22:48:36 -0500

    Update Linker.ex

--- compilador/Linker.ex
+++ compilador/Linker.ex
@@ -4,7 +4,7 @@ defmodule Linker do
     assembly_file_name = Path.basename(assembly_path)
     binary_file_name = Path.basename(assembly_path, ".s")
     output_dir_name = Path.dirname(assembly_path)
-    assembly_path = output_dir_name <> "/" <> assembly_file_name
+   
     File.write!(assembly_path, assembler)
     System.cmd("gcc", [binary_file_name <> ".c", "-o#{binary_file_name}"], cd: output_dir_name)
   end
