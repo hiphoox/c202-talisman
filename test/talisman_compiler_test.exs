216ca2b70025a2a1ad1ab1bd8b0e5ed84760acae
AlanUSB <alanes@comunidad.unam.mx>
2020-06-18 09:04:32 -0500

    Add files via upload

--- test/talisman_compiler_test.exs
+++ test/talisman_compiler_test.exs
@@ -2,10 +2,7 @@ defmodule TalismanCompilerTest do
   use ExUnit.Case
   doctest TalismanCompiler
 
-  test "greets the world" do
-    assert TalismanCompiler.hello() == :world
-  end
+  ##test "greets the world" do
+    ##assert TalismanCompiler.hello() == :world
+  ##end
 end
-
-
-###Falta automatizar la pruebas
